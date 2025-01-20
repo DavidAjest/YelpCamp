da5b9125fa43526325d83dfc9bb75f414f7f5dd9
David Etin <ajest123@gmail.com>
2025-01-20 14:23:07 +0200

    updated db to atlas db

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL; //"mongodb://localhost:27017/yelp-camp";
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 
